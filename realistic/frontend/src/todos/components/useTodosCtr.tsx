77944e83ead7e8df6430f4bee65b2d00c02d31c7
Maarten Nieber <hallomaarten@yahoo.com>
2021-04-15 10:17:43 +0200

    Fix useTodosCtr, use a MobX reaction

--- realistic/frontend/src/todos/components/useTodosCtr.tsx
+++ realistic/frontend/src/todos/components/useTodosCtr.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { runInAction } from 'mobx';
+import { action, reaction } from 'mobx';
 import { TodosCtr } from 'src/todos/TodosCtr';
 import { useStore } from 'src/app/components/StoreProvider';
-import { TodoT } from 'src/todos/types';
+import { TodoT, TodoByIdT } from 'src/todos/types';
 
 const TodosCtrContext = React.createContext<TodosCtr | undefined>(undefined);
 
 export const TodosCtrProvider: React.FC = ({ children }) => {
+  const { todosStore } = useStore();
+
   const [ctr] = React.useState(() => {
     const ctr = new TodosCtr();
     ctr.filtering.apply((todos: TodoT[]) => {
@@ -16,6 +18,19 @@ export const TodosCtrProvider: React.FC = ({ children }) => {
     return ctr;
   });
 
+  React.useEffect(() => {
+    const cleanUp = reaction(
+      () => todosStore.todoById,
+      action((todoById: TodoByIdT) => {
+        ctr.inputs.todoById = todoById;
+      }),
+      {
+        fireImmediately: true,
+      }
+    );
+    return cleanUp;
+  });
+
   return (
     <TodosCtrContext.Provider value={ctr}>{children}</TodosCtrContext.Provider>
   );
@@ -23,17 +38,8 @@ export const TodosCtrProvider: React.FC = ({ children }) => {
 
 export const useTodosCtr = () => {
   const ctr = React.useContext(TodosCtrContext);
-  const { todosStore } = useStore();
-
   if (!ctr) {
     throw new Error('useTodosCtr must be used within a TodosCtrProvider.');
   }
-
-  React.useEffect(() => {
-    runInAction(() => {
-      ctr.inputs.todoById = todosStore.todoById;
-    });
-  }, [ctr.inputs, todosStore.todoById]);
-
   return ctr;
 };
